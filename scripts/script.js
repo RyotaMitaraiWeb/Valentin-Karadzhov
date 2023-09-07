03535699a9597a45b1b76ace8866c5d5dc10b527
RyotaMitaraiWeb <51201292+RyotaMitaraiWeb@users.noreply.github.com>
2023-09-07 16:40:23 +0300

    Add a navigation menu (#1)
    * add links and base styling for desktop
    
    * make the navigation menu functional on small screens

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,41 @@
 window.addEventListener('DOMContentLoaded', () => {
-  
+  const menuButton = document.querySelector('#menu-button');
+  menuButton.addEventListener('click', showNavigationMenu);
+
+  function showNavigationMenu(event) {
+    event.preventDefault();
+    const links = document.querySelector('.links');
+    links.classList.add('visible');
+
+    const body = document.querySelector('body');
+    body.classList.add('locked');
+
+    createOverlay();
+  }
+
+  function createOverlay() {
+    const overlay = document.createElement('div');
+    overlay.classList.add('overlay');
+
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+
+    overlay.addEventListener('click', hideNavigationMenu);
+  }
+
+  function hideNavigationMenu(event) {
+    event.preventDefault();
+    const overlay = document.querySelector('.overlay');
+    const body = document.querySelector('body');
+    const links = document.querySelector('.links');
+
+    overlay.classList.add('hide-overlay');
+    links.classList.add('invisible');
+
+    setTimeout(() => {
+      body.removeChild(overlay);
+      body.classList.remove('locked');
+      links.classList.remove('visible', 'invisible');
+    }, 500);
+  }
 });
