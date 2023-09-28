82e20a524aa466c15ae57465f7e00e91ebca5b11
RyotaMitaraiWeb <Titanium5624@gmail.com>
2023-09-28 22:05:01 +0300

    create a settings menu and move theme switch to that menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,43 @@
 window.addEventListener('DOMContentLoaded', () => {
-  const theme = localStorage.getItem('theme') || 'light';
   setThemeOnLoad();
+  setAnimationPreferencesOnLoad();
 
   createOverlay();
+  createOverlay('settings-overlay')
 
   const menuButton = document.querySelector('#menu-button');
   menuButton.addEventListener('click', showNavigationMenu);
 
-  function setTheme(theme) {
-    const body = document.querySelector('body');
-    if (theme === 'light') {
-      body.classList.add('light');
-      body.classList.remove('dark');
-    } else {
-      body.classList.remove('light');
-      body.classList.add('dark');
-    }
+  const settingsButton = document.querySelector('#settings-button');
+  settingsButton.addEventListener('click', showSettingsMenu);
 
-    const toggle = document.querySelector('#switch');
-    toggle.checked = theme === 'dark';
-  }
+  const themeButtons = document.querySelectorAll('.theme-buttons button');
+  themeButtons.forEach(b => b.addEventListener('click', changeThemeFromButton));
 
+  const animationButtons = document.querySelectorAll('.animation-buttons button');
+  animationButtons.forEach(b => b.addEventListener('click', changeAnimationPreferenceFromButton));
+
+  const menuButtons = document.querySelectorAll('.menu-button');
+  menuButtons.forEach(mb => mb.addEventListener('click', function (e) {
+    e.preventDefault();
+    menuButtons.forEach(mb => mb.disabled = true);
+  }));
 
   const closeMenuButton = document.querySelector('.close-menu');
-  closeMenuButton.addEventListener('click', hideNavigationMenu);
+  closeMenuButton.addEventListener('click', hideMenus);
 
-  function createOverlay() {
+  const closeSettings = document.querySelector('#close-settings');
+  closeSettings.addEventListener('click', hideMenus);
+
+  function createOverlay(...additionalClassNames) {
     const overlay = document.createElement('div');
-    overlay.classList.add('overlay', 'hide-overlay');
+
+    overlay.classList.add('overlay', 'hide-overlay', ...additionalClassNames);
 
     const body = document.querySelector('body');
     body.appendChild(overlay);
 
-    overlay.addEventListener('click', hideNavigationMenu);
+    overlay.addEventListener('click', hideMenus);
   }
 
   function showNavigationMenu(event) {
@@ -47,19 +52,40 @@ window.addEventListener('DOMContentLoaded', () => {
     const overlay = document.querySelector('.overlay');
     overlay.classList.remove('hide-overlay');
 
-
+    const firstLink = document.querySelector('.link');
+    firstLink.focus();
   }
 
-  function hideNavigationMenu(event) {
+  function showSettingsMenu(event) {
     event.preventDefault();
-    const overlay = document.querySelector('.overlay');
+    const settingsMenu = document.querySelector('.settings-menu');
+    settingsMenu.classList.remove('closed');
+
+    const body = document.querySelector('body');
+    body.classList.add('locked');
+
+    const overlay = document.querySelector('.settings-overlay');
+    overlay.classList.remove('hide-overlay');
+
+    const button = settingsMenu.querySelector('button');
+    button.focus();
+  }
+
+  function hideMenus(event) {
+    event.preventDefault();
+    const overlays = document.querySelectorAll('.overlay');
     const body = document.querySelector('body');
     const links = document.querySelector('.links');
 
-    overlay.classList.add('hide-overlay');
+    const settingsMenu = document.querySelector('.settings-menu');
+    settingsMenu.classList.add('closed');
+
+    overlays.forEach(o => o.classList.add('hide-overlay'));
     body.classList.remove('locked');
 
     links.classList.add('invisible');
+
+    menuButtons.forEach(mb => mb.disabled = false);
   }
 
   const body = document.querySelector('body');
@@ -67,42 +93,117 @@ window.addEventListener('DOMContentLoaded', () => {
 
   function setThemeOnLoad() {
     const localStorageTheme = localStorage.getItem('theme');
-    const slideToggle = document.querySelector('#switch');
 
     if (!localStorageTheme) {
-      const preference = prefersDarkMode();
-      setTheme(preference);
-      slideToggle.checked = true;
+      changeTheme('system');
     } else {
-      setTheme(localStorageTheme);
-      slideToggle.checked = localStorageTheme === 'dark';
-    }
-
-    const slideToggleTooltip = document.querySelector('.switch.tooltip .tooltip-text');
-    slideToggleTooltip.addEventListener('click', updateThemeFromToggle);
-
-    slideToggle.addEventListener('click', updateThemeFromToggle);
-  }
-
-  function updateThemeFromToggle() {
-    const localStorageTheme = localStorage.getItem('theme');
-
-    if (!localStorageTheme && prefersDarkMode()) {
-      setTheme('light');
-      localStorage.setItem('theme', 'light');
-    } else if (!localStorageTheme && prefersDarkMode()) {
-      setTheme('light');
-      localStorage.setItem('theme', 'dark');
-    } else {
-      const theme = localStorageTheme === 'light' ? 'dark' : 'light';
-      setTheme(theme);
-      localStorage.setItem('theme', theme);
+      changeTheme(localStorageTheme);
     }
   }
 
-  function prefersDarkMode() {
-    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches 
+  function setAnimationPreferencesOnLoad() {
+    const localStorageOption = localStorage.getItem('animations');
+
+    if (!localStorageOption) {
+      changeAnimationSettings('system');
+    } else {
+      changeAnimationSettings(localStorageOption);
+    }
+  }
+
+  function changeTheme(theme) {
+    const body = document.querySelector('body');
+    if (theme === 'system') {
+      const mediaTheme = mediaThemePreference();
+
+      body.classList.add(mediaTheme);
+      body.classList.remove(mediaTheme === 'light' ? 'dark' : 'light');
+    } else {
+      body.classList.add(theme);
+      body.classList.remove(theme === 'light' ? 'dark' : 'light');
+    }
+
+    const buttons = document.querySelectorAll('.theme-buttons button');
+    buttons.forEach(b => {
+      const buttonTheme = b.dataset.theme;
+      if (buttonTheme === theme) {
+        b.classList.add('selected');
+      } else {
+        b.classList.remove('selected');
+      }
+    });
+  }
+
+  function changeAnimationSettings(option) {
+    const body = document.querySelector('body');
+    if (option === 'system') {
+      const mediaPreference = mediaMotionPreference();
+
+      body.classList.add(mediaPreference);
+      body.classList.remove(mediaPreference === 'animate' ? 'no-animate' : 'animate');
+    } else {
+      body.classList.add(option);
+      body.classList.remove(option === 'animate' ? 'no-animate' : 'animate');
+    }
+
+    const buttons = document.querySelectorAll('.animation-buttons button');
+    buttons.forEach(b => {
+      const buttonOption = b.dataset.option;
+      if (buttonOption === option) {
+        b.classList.add('selected');
+      } else {
+        b.classList.remove('selected');
+      }
+    });
+  }
+
+  function changeThemeFromButton(event) {
+    event.preventDefault();
+
+    const button = this;
+    const theme = button.dataset.theme;
+    changeTheme(theme);
+    localStorage.setItem('theme', theme);
+
+    const buttons = document.querySelectorAll('.theme-buttons button');
+    buttons.forEach(b => {
+      const buttonTheme = b.dataset.theme;
+      if (buttonTheme === theme) {
+        b.classList.add('selected');
+      } else {
+        b.classList.remove('selected');
+      }
+    });
+  }
+
+  function changeAnimationPreferenceFromButton(event) {
+    event.preventDefault();
+
+    const button = this;
+    const option = button.dataset.option;
+    changeAnimationSettings(option);
+    localStorage.setItem('animations', option);
+
+    const buttons = document.querySelectorAll('.animation-buttons button');
+    buttons.forEach(b => {
+      const buttonOption = b.dataset.option;
+      if (buttonOption === option) {
+        b.classList.add('selected');
+      } else {
+        b.classList.remove('selected');
+      }
+    });
+  }
+
+  function mediaThemePreference() {
+    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
       ? 'dark'
       : 'light'
+  }
+
+  function mediaMotionPreference() {
+    return window.matchMedia || window.matchMedia(`(prefers-reduced-motion: reduce)`).matches
+      ? 'no-animate'
+      : 'animate';
   }
 });
