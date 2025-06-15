62f118e20e4378f33f7aedc70c913336a529d544
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-06-15 19:47:48 +0300

    create initial menu

---
+++ src/common/navigation.ts
@@ -0,0 +1,27 @@
+export type NavigationLink = {
+  text: string;
+  href: string;
+};
+
+export const links: NavigationLink[] = [
+  {
+    text: "Home",
+    href: "/",
+  },
+  {
+    text: "Portfolio",
+    href: "/portfolio",
+  },
+  {
+    text: "Tech stack",
+    href: "/skills",
+  },
+  {
+    text: "About me",
+    href: "/about",
+  },
+  {
+    text: "Contact me",
+    href: "/#contacts"
+  },
+];
