f169391e7900081d71f749abd5156e7a0795dc7a
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-06-16 18:10:57 +0300

    remove delay if the user has not allowed animations

--- src/util/dialog/closeDialog.ts
+++ src/util/dialog/closeDialog.ts
@@ -1,3 +1,5 @@
+import { hasNotBlockedAnimations } from "../hasNotBlockedAnimations";
+
 export function closeDialog(target: HTMLDialogElement | string) {
   if (typeof target === "string") {
     target = document.getElementById(target) as HTMLDialogElement;
@@ -5,7 +7,11 @@ export function closeDialog(target: HTMLDialogElement | string) {
 
   target.classList.add("close");
 
-  setTimeout(() => {
+  if (hasNotBlockedAnimations()) {
+    setTimeout(() => {
+      target.close();
+    }, 150);
+  } else {
     target.close();
-  }, 150);
+  }
 }
