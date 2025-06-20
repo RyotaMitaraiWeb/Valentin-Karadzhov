db38bba309004e90cc6b0e5f9ea15b2a48169d52
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-06-21 01:26:47 +0300

    add Quiz World

--- src/common/portfolio.ts
+++ src/common/portfolio.ts
@@ -1,7 +1,8 @@
 import CardflowText from "../content/CardflowText.astro";
 import CardflowBanner from "../assets/images/projects/banners/cardflow.jpg";
-
+import QuizWorldBanner from "../assets/images/projects/banners/quiz-world.jpg";
 import type { AstroComponent } from "./util";
+import QuizWorldText from "../content/QuizWorldText.astro";
 
 export type ExternalLink = {
   href: string;
@@ -44,6 +45,35 @@ export const webApps: Project[] = [
     bannerAlt:
       "A sample from the Cardflow project, showing two users engaging in a trade, which features a chat and an offer tracker.",
   },
+  {
+    id: "quiz-world",
+    title: "Quiz World",
+    description:
+      "An app that allows users to play on and create quizzes. Supports single-choice, multiple-choice, and text-based questions",
+    fullContent: QuizWorldText,
+    banner: QuizWorldBanner,
+    bannerAlt:
+      "A user answering a single choice question correctly. The question features notes which give the user more context",
+    techStack: [
+      "Angular 20",
+      "Angular Material",
+      "ASP.NET 8",
+      "Entity Framework Core 8",
+      "MS SQL Server",
+      "Redis",
+    ],
+    externalLinks: [
+      {
+        href: "https://github.com/RyotaMitaraiWeb/Quiz-World",
+        text: "Front-end GitHub repository"
+      },
+      {
+        href: "https://github.com/RyotaMitaraiWeb/QuizWorld",
+        text: "Back-end GitHub repository"
+      }
+    ]
+  },
 ];
 
-export const cardflow = webApps.find(app => app.id === "cardflow")!;
+export const cardflow = webApps.find((app) => app.id === "cardflow")!;
+export const quizWorld = webApps[1];
